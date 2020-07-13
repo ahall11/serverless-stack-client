afb93c2b2605d6436a4fe11489d05015b265f555
Alex Hall <a_hall11@u.pacific.edu>
2020-07-13 15:30:11 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "notes-app-uploads-ajh"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_ajQnjm97a",
+    APP_CLIENT_ID: "7r38vs1an48uepnfadu9ht1fna",
+    IDENTITY_POOL_ID: "us-west-2:0bca0aca-e914-404a-8c45-b2aeee003354"
+  }
+};
