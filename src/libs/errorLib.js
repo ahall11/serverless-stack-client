bc6e3a0feadf071a185b4721d2a02fe40f10dba7
Alex Hall <a_hall11@u.pacific.edu>
2020-07-18 22:49:39 -0700

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,42 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({dsn: "https://6c3b14e172a34cf6ae32b2150af4d8a7@o422312.ingest.sentry.io/5347629"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
 export function onError(error) {
+  let errorInfo = {};
   let message = error.toString();
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
     message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
   }
+
+  logError(error, errorInfo);
 
   alert(message);
 }
+
